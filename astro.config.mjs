7dea64f8b50db6b74dbca9f3f7f85be4b1153201
tgomilar <tanja.gomilar@gmail.com>
2026-05-30 08:33:34 +0200

    chore: update all URLs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://motion-components.dev",
+  site: "https://www.motion-components.dev",
   outDir: "dist",
   integrations: [sitemap()],
 });
